081cce34d7e201cbb007b5633980b72fb37df6f1
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-03-21 19:08:11 -0400

    Includes trash across screen

--- sketch.js
+++ sketch.js
@@ -2,13 +2,28 @@ function setup() {
   createCanvas(400, 400);
 }
 
+  let xPos = [400];
+  let yPos = [200];
+
 function draw() {
   background(70, 220, 500);
+ 
   
   //trash bin (always where mouse is)
   noStroke();
   fill(100);
-  rect(mouseX-10, mouseY-20, 30, 40);
+  rect(mouseX-20, mouseY-15, 40, 30);
   fill(70);
-  rect(mouseX-12, mouseY-22, 34, 4);
+  rect(mouseX+20, mouseY-17, 4, 34);
+  
+  //trash goes across screen and repeats when reaches left side
+  for (var t = 0; t < 5; t++) {
+  ellipse(xPos[t], yPos[t], 10, 10);
+    xPos.push(400);
+    yPos.push(random(0, 400));
+    xPos[t] -= 2;
+    if (xPos[t] === 0) {
+      xPos[t] = 400;
+    }
+  }
 }
