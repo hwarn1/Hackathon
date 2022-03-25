ed9060d88869e85ca7e760fb32ce35de78218014
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-03-25 11:36:44 -0400

    Object attempts commented out (no real changes)

--- sketch.js
+++ sketch.js
@@ -1,13 +1,33 @@
+//var trash;
+let piece;
+let pieces = [];
+
 function setup() {
   createCanvas(400, 400);
+  
+  /*piece = new trash();
+  
+  for (var i = 0; i < 5; i++) {
+    pieces[i] = new trash(400,random(5), 30, 30);
+  }*/
+  
 }
-
   let xPos = [400];
   let yPos = [200];
 
 function draw() {
   background(70, 220, 500);
  
+    //trash goes across screen and repeats when reaches left side
+  for (var t = 0; t < 5; t++) {
+    ellipse(xPos[t], yPos[t], 10, 10);
+    xPos.push(400);
+    yPos.push(random(0, 400));
+    xPos[t] -= 2;
+    if (xPos[t] === 0) {
+      xPos[t] = 400;
+    } 
+  }
   
   //trash bin (always where mouse is)
   noStroke();
@@ -16,14 +36,4 @@ function draw() {
   fill(70);
   rect(mouseX+20, mouseY-17, 4, 34);
   
-  //trash goes across screen and repeats when reaches left side
-  for (var t = 0; t < 5; t++) {
-  ellipse(xPos[t], yPos[t], 10, 10);
-    xPos.push(400);
-    yPos.push(random(0, 400));
-    xPos[t] -= 2;
-    if (xPos[t] === 0) {
-      xPos[t] = 400;
-    }
-  }
 }
