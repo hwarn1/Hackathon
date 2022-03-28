a21484e713eef39e45c35c9ef96224a4bfd6911b
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-03-28 10:24:06 -0400

    Update sketch.js
    object-oriented trash

--- sketch.js
+++ sketch.js
@@ -1,24 +1,26 @@
-//var trash;
-let piece;
 let pieces = [];
 
 function setup() {
   createCanvas(400, 400);
   
-  /*piece = new trash();
   
   for (var i = 0; i < 5; i++) {
-    pieces[i] = new trash(400,random(5), 30, 30);
-  }*/
+    pieces[i] = new trash(400,random(400), 10, 10);
+  }
   
 }
-  let xPos = [400];
-  let yPos = [200];
 
 function draw() {
   background(70, 220, 500);
- 
-    //trash goes across screen and repeats when reaches left side
+
+   
+  for (var i = 0; i < 5; i++) {
+    pieces[i].show();
+    pieces[i].update();
+  }
+
+  
+    /*//trash goes across screen and repeats when reaches left side
   for (var t = 0; t < 5; t++) {
     ellipse(xPos[t], yPos[t], 10, 10);
     xPos.push(400);
@@ -26,8 +28,8 @@ function draw() {
     xPos[t] -= 2;
     if (xPos[t] === 0) {
       xPos[t] = 400;
-    } 
-  }
+    } /*
+  }*/
   
   //trash bin (always where mouse is)
   noStroke();
@@ -35,5 +37,4 @@ function draw() {
   rect(mouseX-20, mouseY-15, 40, 30);
   fill(70);
   rect(mouseX+20, mouseY-17, 4, 34);
-  
 }
