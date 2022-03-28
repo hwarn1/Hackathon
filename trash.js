6e3576a052f16e6b13ba9404cb4a87198f60cdc2
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-03-28 10:24:34 -0400

    Update trash.js

--- trash.js
+++ trash.js
@@ -1,9 +1,17 @@
 class trash {
   
-  contructor(x,y, width, height) {
-    this.pos = createVector(400+x*1000, (height-50-y*50));
+  constructor(x,y, width, height) {
+    this.pos = createVector(x, y);
+    //(400+x*1000, (height-50-y*50));
     this.width = width;
     this.height = height;
   }
   
+  show() {
+    fill(100,100,100);
+    ellipse(this.pos.x, this.pos.y, this.width, this.height)
+  }
+  update() {
+    this.pos.x -= 2;
+  }
 }
