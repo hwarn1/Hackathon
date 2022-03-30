7d5da84ea96aa8ef9d57e8c336d578dea1ddd045
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-03-30 09:59:37 -0400

    Update trash.js

--- trash.js
+++ trash.js
@@ -13,7 +13,7 @@ class trash {
   }
   update() {
     this.pos.x -= 2;
-    if(this.pos.x >= mouseX && this.pos.x <= mouseX+20 && this.pos.y >= mouseY-17 && this.pos.y <= mouseY+17) {
+    if(this.pos.x >= mouseX && this.pos.x <= mouseX+20 && this.pos.y >= mouseY-17 && this.pos.y <= mouseY+17 || this.pos.x < -10) {
       this.pos.x = 400;
       this.pos.y = random(400);
     }
