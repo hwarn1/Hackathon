cc522442af09261e5c5a25463bb9b5fcb9ee2fc9
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2022-03-30 09:22:59 -0400

    Update sketch.js
    Home screen

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let pieces = [];
+var sceneNum = 1;
 
 function setup() {
   createCanvas(400, 400);
@@ -10,26 +11,29 @@ function setup() {
   
 }
 
-function draw() {
+function home() {
+  sceneNum = 1;
+  background(100, 200, 200);
+  fill(300, 300, 300);
+  textSize(40);
+  text("Save the Ocean!", 50, 150);
+  
+  rect(100, 200, 200, 70);
+  fill(100, 200, 200);
+  text("PLAY", 150, 250);
+  
+}
+
+function game() {
+  sceneNum = 2;
   background(70, 220, 500);
 
    
   for (var i = 0; i < 5; i++) {
     pieces[i].show();
     pieces[i].update();
+    
   }
-
-  
-    /*//trash goes across screen and repeats when reaches left side
-  for (var t = 0; t < 5; t++) {
-    ellipse(xPos[t], yPos[t], 10, 10);
-    xPos.push(400);
-    yPos.push(random(0, 400));
-    xPos[t] -= 2;
-    if (xPos[t] === 0) {
-      xPos[t] = 400;
-    } /*
-  }*/
   
   //trash bin (always where mouse is)
   noStroke();
@@ -37,4 +41,17 @@ function draw() {
   rect(mouseX-20, mouseY-15, 40, 30);
   fill(70);
   rect(mouseX+20, mouseY-17, 4, 34);
+}
+
+function draw() {
+  if (sceneNum === 1) {
+    home();
+  } else if (sceneNum === 2)
+    game();
+}
+
+function mouseClicked() {
+  if (sceneNum === 1 && mouseX >= 100 && mouseX <= 300 && mouseY >= 200 && mouseY <= 270) {
+    sceneNum = 2;
+  }
 }
